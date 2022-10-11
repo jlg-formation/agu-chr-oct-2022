53e3e237a7cd58ca3890d23117c6afb493a6dfb9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-11 09:35:49 +0200

    dev: added wwwDir
    to point to production angular files.

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,6 +5,7 @@ import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
+const wwwDir = "../front/dist/front";
 
 app.use((req, res, next) => {
   console.log("req: ", req.url);
@@ -15,8 +16,8 @@ app.get("/api/date", (req, res) => {
   res.json({ date: new Date() });
 });
 
-app.use(express.static("."));
-app.use(serveIndex(".", { icons: true }));
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
