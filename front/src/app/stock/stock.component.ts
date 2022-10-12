0a56f2ae7b6c34121f779e01314e3782c6b8f209
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-12 09:53:40 +0200

    dev: *ngFor
    We have create an articles attributes and we loop on it.

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/interfaces/Article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    {
+      name: 'Tournevis',
+      price: 3.45,
+      qty: 100,
+    },
+    {
+      name: 'Tournevis cruciforme',
+      price: 4.5,
+      qty: 100,
+    },
+    {
+      name: 'Tondeuse à gazon',
+      price: 250,
+      qty: 3,
+    },
+    {
+      name: 'Pelle',
+      price: 5,
+      qty: 150,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
