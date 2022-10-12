47080994155fd137967cdd378f49630060049110
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-12 10:39:01 +0200

    dev: started form

--- front/src/app/stock/create/create.component.ts
+++ front/src/app/stock/create/create.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Truc', [Validators.required]),
+    price: new FormControl('1', [Validators.required]),
+    qty: new FormControl(1, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit() {
+    console.log('submit');
+    this.router.navigate(['..'], { relativeTo: this.route });
   }
-
 }
