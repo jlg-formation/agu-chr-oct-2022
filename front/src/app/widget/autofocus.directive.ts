82b85fbafa2aef209dcc7929a9827ee1e07cfd0a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-13 14:03:10 +0200

    dev: added input to the directive

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,14 +1,26 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
 export class AutofocusDirective implements OnInit {
+  @Input()
+  selectAll: string = 'noselect';
+
   constructor(private elt: ElementRef<HTMLElement>) {
     console.log('instantiate directive autofocus');
   }
 
   ngOnInit(): void {
+    console.log('this.selectAll: ', this.selectAll);
+    if (
+      this.selectAll !== 'noselect' &&
+      this.elt.nativeElement instanceof HTMLInputElement
+    ) {
+      this.elt.nativeElement.select();
+      return;
+    }
+
     this.elt.nativeElement.focus();
   }
 }
